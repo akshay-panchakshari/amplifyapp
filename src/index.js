ea75da8a6a443962e17e0fa8c7c1ad882a1bf03b
Akshay Panchakshari <panchakshari.akshay@gmail.com>
2020-11-12 11:42:35 +0530

    added comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//amplify imports
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 
